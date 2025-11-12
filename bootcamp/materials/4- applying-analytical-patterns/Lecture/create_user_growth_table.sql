bab4a7cddc5f61de67111f1db6576e4c981a5623
chayuthpong31 <chayuthpong31@gmail.com>
2025-11-12 15:29:16 +0700

    edit lecture

--- bootcamp/materials/4- applying-analytical-patterns/Lecture/create_user_growth_table.sql
+++ bootcamp/materials/4- applying-analytical-patterns/Lecture/create_user_growth_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users_growth_accounting(
 	user_id TEXT,
-	first_active_date TEXT,
+	first_active_date DATE,
 	last_active_date DATE,
 	daily_active_state TEXT,
 	weekly_active_state TEXT,
